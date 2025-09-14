7575adadd88cd86f3bb4e35bca29547cac158e4a
Snak-fpb-sur <snak.gatepia@gmail.com>
2025-09-14 18:17:47 +0900

    bureausを追加

---
+++ app/models/blog/bureau.rb
@@ -0,0 +1,2 @@
+class Blog::Bureau < ApplicationRecord
+end
---
+++ test/models/blog/bureau_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Blog::BureauTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
