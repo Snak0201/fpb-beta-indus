fe1100612b74d1511f6b70e4b6f96ba524aa4e4e
Snak-fpb-sur <snak.gatepia@gmail.com>
2025-09-15 15:56:39 +0900

    トップページのコントローラを追加する

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "static_pages#index"
+  get "static_pages/index"
+
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
