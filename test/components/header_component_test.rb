7ab8d1accef827b1e662958af2077c798f2e77e6
Snak-fpb-sur <snak.gatepia@gmail.com>
2025-09-15 18:50:50 +0900

    rubocop

--- test/components/header_component_test.rb
+++ test/components/header_component_test.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
+
 require "test_helper"
 
 class HeaderComponentTest < ViewComponent::TestCase
-  test 'ホームへのURLが含まれること' do
+  test "ホームへのURLが含まれること" do
     render_inline(HeaderComponent.new)
 
     assert_selector "a[href='/']", text: "ホーム"
